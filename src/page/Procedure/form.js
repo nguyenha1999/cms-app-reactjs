3e70f6ceb6ea7c2b32ab0c2869a5d43e1757895c
nguyenha1999 <kngocha15999@gmail.com>
2021-07-22 08:58:01 +0700

    update code conflict

--- src/page/Procedure/form.js
+++ src/page/Procedure/form.js
@@ -19,65 +19,65 @@ const Form = ({ handleClose, action, item }) => {
     const [hasMore, setHasMore] = useState(true);
 
     useEffect(() => {
-        dispatch(getListDocumnet(model))
+        dispatch(getListDocument(model))
     }
-    }, [model.page])
+        , [model])
 
     useEffect(() => {
         if (!item) return;
         setIdSelected(item.documents.map(x => x.document.id));
     }, [item])
 
-const chageSelected = (id) => {
-    if (idSelected.includes(id)) {
-        setIdSelected(idSelected.filter(x => x != id));
-    } else {
-        setIdSelected([id, ...idSelected]);
+    const chageSelected = (id) => {
+        if (idSelected.includes(id)) {
+            setIdSelected(idSelected.filter(x => x != id));
+        } else {
+            setIdSelected([id, ...idSelected]);
+        }
     }
-}
 
-const checkAll = () => {
-    if (idSelected.length == items.length) {
-        setIdSelected([]);
-    } else {
-        setIdSelected(items.map(x => x.id));
+    const checkAll = () => {
+        if (idSelected.length == items.length) {
+            setIdSelected([]);
+        } else {
+            setIdSelected(items.map(x => x.id));
+        }
     }
-}
 
-const onSubmit = () => {
-    handleClose();
-};
+    const onSubmit = () => {
+        handleClose();
+    };
 
-return (
-    <div className="position-relative">
-        <Row gutter={16}>
-            <Col md={24}>
-                <div className="procedure-infinite-container">
-                    <Checkbox onChange={checkAll}
-                        checked={idSelected.length == items.length}>
-                        Tất cả
-                    </Checkbox>
-                    <List
-                        dataSource={items}
-                        renderItem={item => (
-                            <List.Item key={item.id} className="pointer"
-                                onClick={() => chageSelected(item.id)}
-                            >
-                                <Checkbox checked={idSelected.includes(item.id)} />
-                                <span>{item.docname}</span>
-                            </List.Item>
-                        )}
-                    >
-                    </List>
-                </div>
-            </Col>
-        </Row>
-        <div className="d-flex mt-2">
-            <Button text={action == "add" ? "Thêm" : "Sửa"} className="ml-auto"
-                onClick={() => onSubmit()}
-            />
+    return (
+        <div className="position-relative">
+            <Row gutter={16}>
+                <Col md={24}>
+                    <div className="procedure-infinite-container">
+                        <Checkbox onChange={checkAll}
+                            checked={idSelected.length == items.length}>
+                            Tất cả
+                        </Checkbox>
+                        <List
+                            dataSource={items}
+                            renderItem={item => (
+                                <List.Item key={item.id} className="pointer"
+                                    onClick={() => chageSelected(item.id)}
+                                >
+                                    <Checkbox checked={idSelected.includes(item.id)} />
+                                    <span>{item.docname}</span>
+                                </List.Item>
+                            )}
+                        >
+                        </List>
+                    </div>
+                </Col>
+            </Row>
+            <div className="d-flex mt-2">
+                <Button text={action == "add" ? "Thêm" : "Sửa"} className="ml-auto"
+                    onClick={() => onSubmit()}
+                />
+            </div>
         </div>
-    </div>
-)
+    )
 }
 export default Form;
